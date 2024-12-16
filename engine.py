59e9dc9331fb71c1b568189bb30dc6afb84062da
TheSalamanderDJ <thesalamanderdj@gmail.com>
2024-12-16 16:16:29 -0500

    Added inventory and health potion functionality

--- engine.py
+++ engine.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from tcod.console import Console
 from tcod.map import compute_fov
 
+import exceptions
 from entity import Actor
 from game_map import GameMap
 from input_handlers import MainGameEventHandler
@@ -29,7 +30,10 @@ class Engine:
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
-                entity.ai.perform()
+                try:
+                    entity.ai.perform()
+                except exceptions.Impossible:
+                    pass  # Ignore impossible action exceptions in AI.
 
     def update_fov(self) -> None:
         """Recompute the visible area based on the players point of view."""
